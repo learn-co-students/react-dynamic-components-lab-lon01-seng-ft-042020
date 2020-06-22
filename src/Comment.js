f9b26a031b3359fbd1515cf32281516e95b3ee95
ewelinaszoda <ewelinaszoda@gmail.com>
2020-06-22 20:49:33 +0100

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,11 @@
 //your code here
+import React, { Component } from 'react';
+
+// export the class so it can be used in other files ???? DEFAULT??? line 5 or line 11
+class Comment extends Component {
+  render() {
+    return <div className="comment">{this.props.commentText}</div>;
+  }
+}
+
+export default Comment;
