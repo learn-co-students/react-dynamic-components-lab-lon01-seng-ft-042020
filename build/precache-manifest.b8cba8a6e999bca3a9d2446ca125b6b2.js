145b0c5341064847deb82515231921de324afeb7
zbadiru <zbadiru@live.co.uk>
2020-06-23 10:58:39 +0100

    Done.

---
+++ build/precache-manifest.b8cba8a6e999bca3a9d2446ca125b6b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "562d283ea15ee8eba377",
+    "url": "/static/css/main.abae50c8.chunk.css"
+  },
+  {
+    "revision": "562d283ea15ee8eba377",
+    "url": "/static/js/main.cd356613.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "cfa5570da706a19eacac31ed4306471b",
+    "url": "/index.html"
+  }
+];
